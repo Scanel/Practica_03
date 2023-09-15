49198129b8f1e470888b382f565490fdc026da0c
César Fierro <79662848+Scanel@users.noreply.github.com>
2023-09-15 09:43:17 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:practica_04/src/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Practica 03', home: HomePage());
+  }
+}
